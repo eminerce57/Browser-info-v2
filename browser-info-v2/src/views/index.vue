2fe2dc7cff46eec3301ab757d111becd78bc4532
Emin Erce <muhammeteminerce@gmail.com>
2024-03-02 20:14:07 +0300

    Update index.vue

--- browser-info-v2/src/views/index.vue
+++ browser-info-v2/src/views/index.vue
@@ -92,7 +92,7 @@ const info = reactive({
   ScreenOrientation: window.screen.orientation.type,
   TouchPoints: navigator.maxTouchPoints || 0,
   MediaDevices: getMediaDevices(),
-    Battery: getBatteryInfo() // Pil bilgisi
+    Battery: getBatteryInfo() 
 });
 const win = ref('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-windows" viewBox="0 0 16 16"> <path d="M6.555 1.375 0 2.237v5.45h6.555V1.375zM0 13.795l6.555.933V8.313H0v5.482zm7.278-5.4.026 6.378L16 16V8.395H7.278zM16 0 7.33 1.244v6.414H16V0z"/> </svg>')
 const mac = ref('<svg xmlns="http://www.w3.org/2000/svg" width="0.82em" height="1em" viewBox="0 0 256 315"><path d="M213.803 167.03c.442 47.58 41.74 63.413 42.197 63.615c-.35 1.116-6.599 22.563-21.757 44.716c-13.104 19.153-26.705 38.235-48.13 38.63c-21.05.388-27.82-12.483-51.888-12.483c-24.061 0-31.582 12.088-51.51 12.871c-20.68.783-36.428-20.71-49.64-39.793c-27-39.033-47.633-110.3-19.928-158.406c13.763-23.89 38.36-39.017 65.056-39.405c20.307-.387 39.475 13.662 51.889 13.662c12.406 0 35.699-16.895 60.186-14.414c10.25.427 39.026 4.14 57.503 31.186c-1.49.923-34.335 20.044-33.978 59.822M174.24 50.199c10.98-13.29 18.369-31.79 16.353-50.199c-15.826.636-34.962 10.546-46.314 23.828c-10.173 11.763-19.082 30.589-16.678 48.633c17.64 1.365 35.66-8.964 46.64-22.262"/></svg>')
